8a8f90c39982c20eaa079a06bd05639447ad3964
usama learner <syedusamalearner@gmail.com>
2020-07-13 00:08:58 +0500

    initiall

---
+++ build/precache-manifest.0b57d0039ad02e1a0f5d8b5be60eaefa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c99bb2e86307ca114b57f055b6f62a49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4bd65402780714928de",
+    "url": "/static/css/main.13c8a689.chunk.css"
+  },
+  {
+    "revision": "5baeb59ab6a76fa3adc5",
+    "url": "/static/js/2.1ccdc7e0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.1ccdc7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bd65402780714928de",
+    "url": "/static/js/main.5115a17a.chunk.js"
+  },
+  {
+    "revision": "649da48e7cc251d62abb",
+    "url": "/static/js/runtime-main.f6aca1be.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
